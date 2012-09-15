1da0779c8fca5a17be939316e535ffa747a7396d
Murphy Randle <murphyspublic@gmail.com>
2012-09-14 22:23:39 -0600

    Broke build. Revised.

---
+++ src/gui/reports/common/IReport.java
@@ -0,0 +1,8 @@
+package gui.reports.common;
+
+/**
+ * Base class for all of the report controllers. Report controllers will 
+ * collect all of the information they need, and then ask the 
+ * {@link gui.reports.common.Renderer Renderer} to render them out.
+ */
+public class IReport {}
