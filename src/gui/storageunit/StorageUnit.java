09226e66b9cc0bd2dfa12f4662478539dfa5324f
Nick Ethier <ncethier@gmail.com>
2012-09-14 20:10:49 -0600

    Add storageunit and productgroup javadoc

--- src/gui/storageunit/StorageUnit.java
+++ src/gui/storageunit/StorageUnit.java
@@ -4,14 +4,36 @@ import gui.common.IModel;
 import common.Result;
 import gui.inventory.ProductContainerData;
 
+/**
+ * The StorageUnit class encapsulates all the funtions and data associated with a "StorageUnit".
+ * It extends the {@link gui.inventory.ProductContainerData ProductContainerData} 
+ * 	class which contains getters and setters for the various datas.
+ */
 public class StorageUnit extends ProductContainerData implements IModel{
 
+	/**
+	 * A unique ID is associated with every StorageUnit once it is presisted to the vault.
+	 * _id is not set by the user, but by the vault when it is saved.
+	 * _id can be -1 if it is new and has not been saved
+	 */
 	private int _id;
 
+	/**
+	 * When a change is made to the data it becomes invalid and 
+	 * must be validated before it can be saved.
+	 * _valid maintains this state
+	 */
 	private boolean _valid;
 
+	/**
+	 * _saved maintaines the state of if the instance of the model is the same as the 
+	 * persisted model in the vault.
+	 */
 	private boolean _saved;	
 
+	/**
+	 * Constructor
+	 */
 	public StorageUnit(){
 		super();
 		_id = -1;
@@ -19,18 +41,30 @@ public class StorageUnit extends ProductContainerData implements IModel{
 		_saved = false;
 	}
 
+	/**
+	 * Is the StorageUnit saved?
+	 */
 	public boolean isSaved(){
 		return this._saved;
 	}
 
+	/**
+	 * Is the StorageUnit valid?
+	 */
 	public boolean isValid(){
 		return this._valid;
 	}
 
+	/**
+	 * If the StorageUnit is valid it is saved into the vault.
+	 */
 	public Result save(){
 		return new Result(false, "Saving is not yet implemented");
 	}
 
+	/**
+	 * Validate that the StorageUnit is able to be saved into the vault.
+	 */
 	public Result validate(){
 		return new Result(false, "Validating is not yet implemented");
 	}
