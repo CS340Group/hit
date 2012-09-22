6c37abc716a5780cde440a1a9bb38ae32c47f3b3
Murphy Randle <murphyspublic@gmail.com>
2012-09-22 00:32:33 -0600

    Added some prelimenary unit tests.

--- src/model/productcontainer/ProductContainer.java
+++ src/model/productcontainer/ProductContainer.java
@@ -11,44 +11,42 @@ import common.Result;
  */
 public class ProductContainer implements IModel{
 
-	/**
-	 * A string that is non-empty and must be unique among its vault context.
-	 */
-	private String _name;
-
-	/**
-	 * A unique ID is associated with every ProductContainer once it is presisted to the vault.
-	 * _id is not set by the user, but by the vault when it is saved.
-	 * _id can be -1 if it is new and has not been saved
-	 */
-	private int _id;
-
-	/**
-	 * When a change is made to the data it becomes invalid and 
-	 * must be validated before it can be saved.
-	 * _valid maintains this state
-	 */
-	private boolean _valid;
-
-	/**
-	 * _saved maintaines the state of if the instance of the model is the same as the 
-	 * persisted model in the vault.
-	 */
-	private boolean _saved;	
-
+    /**
+     * A string that is non-empty and must be unique among its vault context.
+     */
     private String _name;
 
-	/**
-	 * Constructor
-	 */
-	public ProductContainer(String name){
-		_name = name;
-		_id = -1;
-		_valid = false;
-		_saved = false;
-	}
+    /**
+     * A unique ID is associated with every ProductContainer once it is presisted to the vault.
+     * _id is not set by the user, but by the vault when it is saved.
+     * _id can be -1 if it is new and has not been saved
+     */
+    private int _id;
 
     /**
+     * When a change is made to the data it becomes invalid and 
+     * must be validated before it can be saved.
+     * _valid maintains this state
+     */
+    private boolean _valid;
+
+    /**
+     * _saved maintaines the state of if the instance of the model is the same as the 
+     * persisted model in the vault.
+     */
+    private boolean _saved;
+
+    /**
+     * Constructor
+     */
+    public ProductContainer(){
+        _name = "";
+        _id = -1;
+        _valid = false;
+        _saved = false;
+    }
+
+        /**
      * Copy Constructor
      */
     public ProductContainer(ProductContainer p){
@@ -57,52 +55,54 @@ public class ProductContainer implements IModel{
         _saved = false;
     }
 
-    public int getId(){
-        return _id;
-    }
-
-    public String getName(){
+    /**
+     * Getters and setters:
+     */
+    public String getName() {
         return _name;
     }
 
-    public Result setName(String n){
-        _name = n;
-        return new Result(true);
+    public void setName(String _name) {
+        this._name = _name;
     }
 
-	/**
-	 * Is the ProductContainer saved?
-	 */
-	public boolean isSaved(){
-		return this._saved;
-	}
+    /**
+     * Is the ProductContainer saved?
+     */
+    public boolean isSaved(){
+        return this._saved;
+    }
 
-	/**
-	 * Is the ProductContainer valid?
-	 */
-	public boolean isValid(){
-		return this._valid;
-	}
+    /**
+     * Is the ProductContainer valid?
+     */
+    public boolean isValid(){
+        return this._valid;
+    }
 
-	/**
-	 * If the ProductContainer is valid it is saved into the vault.
-	 */
-	public Result save(){
-		if(!isValid()){
-			return new Result(false, "Not yet validated.");
-		}else{
-            return new Result(false, "foo");
+    /**
+     * If the ProductContainer is valid it is saved into the vault.
+     */
+    public Result save(){
+        if(!isValid()){
+            return new Result(false, "Not yet validated.");
         }
-	}
+                else {
+                    return new Result(true, "Will soon be implemented.");
+                }
+    }
 
-	/**
-	 * Validate that the ProductContainer is able to be saved into the vault.
-	 */
-	public Result validate(){
-		if(_name == ""){
-			return new Result(false, "Name must be non-empty.");
-		}
-		return new Result(false, "Validating is not yet implemented");
-	}
-	
+    /**
+     * Validate that the ProductContainer is able to be saved into the vault.
+     */
+    public Result validate(){
+        if(_name == ""){
+            return new Result(false, "Name must be non-empty.");
+        }
+        else {
+            _valid = true;
+            return new Result(true, "Validating is not yet implemented");
+        }
+    }
+    
 }
