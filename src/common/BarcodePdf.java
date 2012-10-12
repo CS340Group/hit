b0444af5b0fa8a6b81755c9c4e5ab6fb109469a6
Murphy Randle <murphyspublic@gmail.com>
2012-10-12 14:22:06 -0600

    Added convenience methods to item. Barcode printer almost done.

--- src/common/BarcodePdf.java
+++ src/common/BarcodePdf.java
@@ -3,16 +3,22 @@ package common;
 import java.io.FileOutputStream;
 import java.io.IOException;
 
-import model.common.Barcode;
+import org.joda.time.LocalDate;
+
+import model.item.Item;
 
 import com.itextpdf.text.Document;
 import com.itextpdf.text.DocumentException;
 import com.itextpdf.text.Element;
+import com.itextpdf.text.Chunk;
+import com.itextpdf.text.Font;
 import com.itextpdf.text.Image;
+import com.itextpdf.text.Phrase;
 import com.itextpdf.text.pdf.BarcodeEAN;
 import com.itextpdf.text.pdf.PdfContentByte;
 import com.itextpdf.text.pdf.PdfWriter;
-import com.itextpdf.text.pdf.ColumnText;
+import com.itextpdf.text.pdf.PdfPTable;
+import com.itextpdf.text.pdf.PdfPCell;
 
 
 public class BarcodePdf{
@@ -21,96 +27,72 @@ public class BarcodePdf{
 	private PdfWriter _writer;
 	private Boolean _open;
 	private PdfContentByte _rawContent;
-	private ColumnText _ct;
-	private int _curcol;
-	private int _colstatus;
-	
-    /** Definition of two columns */
-    public static final float[][] COLUMNS = {
-        { 36, 36, 296, 806 } , { 299, 36, 559, 806 }
-    };
-	
+	private PdfPTable _table;
+
+
 	public BarcodePdf(String fname){
 
 		_d = new Document();
 		
 		try{
 			_writer = PdfWriter.getInstance(_d, new FileOutputStream(fname));
+			
+			_d.setMargins(25, 25, 25, 25);
 			_d.open();
+			
 			_open = true;
 			_rawContent = _writer.getDirectContent();
+			
+			_table = new PdfPTable(4);
+			_table.setHorizontalAlignment(Element.ALIGN_CENTER);
+			
 		} catch (DocumentException dexc){
 			System.out.println("A new document could not be created.");
 		} catch (IOException e) {
 			System.out.println("Sorry, that's not a valid file path.");
 		}
 
-		/* Copied from the example */
-        _ct = new ColumnText(_rawContent);
-        _ct.setAlignment(Element.ALIGN_JUSTIFIED);
-        _ct.setExtraParagraphSpace(6);
-        _ct.setLeading(14);
-        _ct.setIndent(10);
-        _ct.setRightIndent(3);
-        _ct.setSpaceCharRatio(PdfWriter.NO_SPACE_CHAR_RATIO);
-        _curcol = 0;
-        _colstatus = ColumnText.START_COLUMN;
-        _ct.setSimpleColumn(
-            COLUMNS[_curcol][0], COLUMNS[_curcol][1],
-            COLUMNS[_curcol][2], COLUMNS[_curcol][3]);
-
 	}
 	
-	public Result addBarcode(Barcode b){
+	public Result addItem(Item item){
 		if (!_open){
-			return new Result(false, "Cannot add a barcode to an unopened file.");
+			return new Result(false, "Cannot add a product to an unopened file.");
 		}
-		if (!b.isValid()){
-			return new Result(false, "Cannot print an invalid barcode.");
-		}
+//		if (!product.isValid()){
+//			return new Result(false, "Cannot print an invalid product.");
+//		}
 
 		// Generate the barcode image:
 		BarcodeEAN codeEAN = new BarcodeEAN();
 		codeEAN.setCodeType(com.itextpdf.text.pdf.Barcode.UPCA);
-		codeEAN.setCode(b.toString());
+		codeEAN.setCode(item.getBarcode().toString());
 		Image bcimage = codeEAN.createImageWithBarcode(_rawContent, null, null);
+		
+		// Add some text to a chunk, and the image to a chunk.
+		Font font = new Font(Font.FontFamily.HELVETICA, 8);
+		Chunk c1 = new Chunk(item.getProductDescription() + "\n" + item.getShortEntryDateString() + " -> " + item.getShortExpirationDateString() + "\n\n");
+		c1.setFont(font);
+		Phrase p = new Phrase(c1);
+		Chunk c2 = new Chunk(bcimage, 0, 0);
+		p.add(c2);
 
-		// Add it to the column:
-		_ct.addElement(bcimage);
-		try {
-			_colstatus = _ct.go();
-		} catch (DocumentException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-        if (ColumnText.hasMoreText(_colstatus)) {
-            _curcol = Math.abs(_curcol - 1);
-            if (_curcol == 0)
-                _d.newPage();
-            _ct.setSimpleColumn(
-                COLUMNS[_curcol][0], COLUMNS[_curcol][1],
-                COLUMNS[_curcol][2], COLUMNS[_curcol][3]);
-            _ct.setYLine(COLUMNS[_curcol][3]);
-            try {
-				_colstatus = _ct.go();
-			} catch (DocumentException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-        }
-
-		// try {
-		// 	_d.add(codeEAN.createImageWithBarcode(_rawContent, null, null));
-		// } catch (DocumentException e) {
-		// 	e.printStackTrace();
-		// }
-		// System.out.println("I've added a barcode.");
+		PdfPCell cell = new PdfPCell(p);
+		cell.setHorizontalAlignment(Element.ALIGN_CENTER);
+		cell.setPadding(10);
+		cell.setBorder(0);
+		_table.addCell(cell);
 
 		return new Result(true, "Barcode added successfully.");
 	}
 	
 	public void finish(){
 		if (_open){
+			try {
+				_d.add(_table);
+			} catch (DocumentException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			_d.close();
 			_open = false;
 		}
--- src/model/tempmain/TempMain.java
+++ src/model/tempmain/TempMain.java
@@ -1,50 +1,50 @@
 package model.tempmain;
 
 import java.io.IOException;
-import model.common.Barcode;
 import common.Result;
 import common.BarcodePdf;
 import java.io.File;
 
+import org.joda.time.DateTime;
+
+import model.common.Barcode;
+import model.common.Size;
+import model.item.Item;
+import model.product.Product;
+import model.productcontainer.StorageUnit;
+
 public class TempMain {
 	public static void main(final String[] args) {
 
-		Barcode foo = new Barcode();
-		Result r = foo.setCode("036000291452");
-		r = foo.validate();
+        StorageUnit su = new StorageUnit();
+        su.setName("Test");
+        su.validate();
+        su.save();
+
+        Product product = new Product();
+        product.setDescription("Spam and Eggs");
+        product.set3MonthSupply(3);
+        product.setBarcode(new Barcode("1323"));
+        product.setContainerId(su.getId());
+        product.setCreationDate(new DateTime());
+        product.setShelfLife(2);
+        product.setSize(new Size(3, Size.Unit.oz));
+        product.setStorageUnitId(su.getId());
+        Result valid = product.validate();
+        Result saved = product.save();
+        
+        Item item = new Item();
+        item.setBarcode(new Barcode("1234"));
+        item.setProductId(product.getId());
+        item.setEntryDate(new DateTime());
+        item.setExitDate(new DateTime());
+        item.setExpirationDate(new DateTime().plusMonths(4)); 
+		
 		String fileloc = "/Users/murphyra/spam.pdf";
 		BarcodePdf d = new BarcodePdf(fileloc);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
-		d.addBarcode(foo);
+		for(int i=0; i<100; i++){
+			d.addItem(item);
+		}
 		d.finish();
 		try{
 			java.awt.Desktop.getDesktop().open(new File(fileloc));
