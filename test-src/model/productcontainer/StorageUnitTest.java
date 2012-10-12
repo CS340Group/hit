866c7d4570db676e72517b1817c88a5af7e6fdcc
Brendon Beebe <brendon.beebe@gmail.com>
2012-10-11 23:21:42 -0600

    Fixed build error

--- test-src/model/productcontainer/StorageUnitTest.java
+++ test-src/model/productcontainer/StorageUnitTest.java
@@ -71,8 +71,7 @@ public class StorageUnitTest {
 
     @Test
     public void testIsDeleteable() throws Exception {
-        assertTrue(su1.isDeleteable().getStatus());
-    }
+        assertTrue(su1.isDeletable().getStatus());    }
 
 
 }
