dccd184974dceebbd7421f85caedbac8a8b27327
Nick Ethier <ncethier@gmail.com>
2012-11-04 00:52:19 -0600

    add operator factory tests

--- test-src/model/common/operator/OperatorFactoryTest.java
+++ test-src/model/common/operator/OperatorFactoryTest.java
@@ -5,6 +5,7 @@ import model.common.operator.OperatorFactory;
 import org.junit.Test;
 import org.junit.Before; 
 import org.junit.After;
+import org.joda.time.DateTime;
 
 import static model.common.operator.OperatorFactory.getOperator;
 import static org.junit.Assert.assertFalse;
@@ -24,50 +25,123 @@ public class OperatorFactoryTest {
     public void testEqualToStringOperator() throws Exception {
         String string1 = "hello";
         String string2 = "world";
+        String string3 = "hello ";
 
         Operator op = getOperator("=", string1.getClass());
         assertTrue(op.execute(string1,string1));
+        assertFalse(op.execute(string1,string2));
+        assertFalse(op.execute(string1,string3));
 
     }
 
     @Test
     public void testNotEqualToStringOperator() throws Exception {
-        //TODO: Test goes here...
+        String string1 = "hello";
+        String string2 = "world";
+        String string3 = "hello ";
+
+        Operator op = getOperator("!=", string1.getClass());
+        assertFalse(op.execute(string1,string1));
+        assertTrue(op.execute(string1,string2));
+        assertTrue(op.execute(string1,string3));
     }
 
     @Test
     public void testEqualToDateTimeOperator() throws Exception {
-        //TODO: Test goes here...
+        DateTime time1 = DateTime.now();
+        DateTime time2 = time1.minusSeconds(1);
+
+        Operator op = getOperator("=", time1.getClass());
+        assertTrue(op.execute(time1,time1));
+        assertFalse(op.execute(time1, time2));
     }
 
     @Test
     public void testLessThanDateTimeOperator() throws Exception {
-        //TODO: Test goes here...
+        DateTime time1 = DateTime.now();
+        DateTime time2 = time1.minusSeconds(1);
+
+        Operator op = getOperator("<", time1.getClass());
+        assertTrue(op.execute(time2,time1));
+        assertFalse(op.execute(time1, time1));
     }
 
     @Test
     public void testGreaterThanDateTimeOperator() throws Exception {
-        //TODO: Test goes here...
+        DateTime time1 = DateTime.now();
+        DateTime time2 = time1.minusSeconds(1);
+
+        Operator op = getOperator(">", time1.getClass());
+        assertTrue(op.execute(time1,time2));
+        assertFalse(op.execute(time1, time1));
     }
 
     @Test
     public void testEqualToNumberOperator() throws Exception {
-        //TODO: Test goes here...
+        int i = 1;
+        float f = 1;
+        double d = 1.01;
+
+        Operator op1 = getOperator("=", ((Integer)i).getClass());
+        Operator op2 = getOperator("=", ((Double)d).getClass());
+
+        assertTrue(op1.execute(i,f));
+        assertFalse(op1.execute(i,d));
+        assertFalse(op1.execute(f,d));
+
+        assertTrue(op2.execute(i,f));
+        assertFalse(op2.execute(i,d));
+        assertFalse(op2.execute(f,d));
     }
 
     @Test
     public void testNotEqualToNumberOperator() throws Exception {
-        //TODO: Test goes here...
+        int i = 1;
+        float f = 1;
+        double d = 1.01;
+
+        Operator op1 = getOperator("!=", ((Integer)i).getClass());
+        Operator op2 = getOperator("!=", ((Double)d).getClass());
+
+        assertFalse(op1.execute(i,f));
+        assertTrue(op1.execute(i,d));
+        assertTrue(op1.execute(f,d));
+
+        assertFalse(op2.execute(i,f));
+        assertTrue(op2.execute(i,d));
+        assertTrue(op2.execute(f,d));
     }
 
     @Test
     public void testLessThanNumberOperator() throws Exception {
-        //TODO: Test goes here...
+        int i = 1;
+        float f = 1;
+        double d = 1.01;
+
+        Operator op = getOperator("<", ((Object)i).getClass());
+
+        assertFalse(op.execute(i,f));
+        assertFalse(op.execute(f,i));
+        assertTrue(op.execute(i,d));
+        assertTrue(op.execute(f,d));
+        assertFalse(op.execute(d,i));
+        assertFalse(op.execute(d,f));
     }
 
     @Test
     public void testGreaterThanNumberOperator() throws Exception {
-        //TODO: Test goes here...
+        int i = 1;
+        float f = 1;
+        double d = 1.01;
+
+        Operator op = getOperator(">", ((Object)i).getClass());
+
+        assertFalse(op.execute(i,f));
+        assertFalse(op.execute(f,i));
+        assertFalse(op.execute(i,d));
+        assertFalse(op.execute(f,d));
+        assertTrue(op.execute(d,i));
+        assertTrue(op.execute(d,f));
     }
 
 
