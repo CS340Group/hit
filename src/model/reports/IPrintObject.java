0894cc6d6b4a5caea9db4c16d2f20ec57bff158b
Brendon Beebe <brendon.beebe@gmail.com>
2012-11-05 17:11:09 -0700

    Moved files around

---
+++ src/model/reports/IPrintObject.java
@@ -0,0 +1,46 @@
+package model.reports;
+
+import java.util.ArrayList;
+
+/**
+ * This is the interface for a printable report object.
+ * The report object consists of 3 difference objects:
+ *  - {@link model.reports.IPrintObject.IPrintObjectHeader headers}
+ *  - {@link model.reports.IPrintObject.IPrintObjectTextBlock text-blocks}
+ *  - {@link model.reports.IPrintObject.IPrintObjectTable tables}
+ *
+ *  The PrintObject will reference these object by the order in which they are built by the builder.
+ *  The index starts at 0
+ *
+ *  This means that getHeader(1) will return the second header built by the builder.
+ *
+ */
+
+public interface IPrintObject {
+
+    public IPrintObjectHeader getHeader(int position);
+
+    public ArrayList<IPrintObjectHeader> getHeaders();
+
+    public IPrintObjectTextBlock getTextBlock(int position);
+
+    public ArrayList<IPrintObjectTextBlock> getTextBlocks();
+
+    public IPrintObjectTable getTable(int position);
+
+    public ArrayList<IPrintObjectTable> getTables();
+
+    public interface IPrintObjectHeader {
+        public String toString();
+    }
+
+    public interface IPrintObjectTextBlock {
+        public String toString();
+    }
+
+    public interface IPrintObjectTable {
+        public String getCell(int row, int column);
+
+		public void addRow(String[] row);
+    }
+}
