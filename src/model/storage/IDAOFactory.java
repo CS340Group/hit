ebf6514b94d8f2247f568436124399690763cef4
Murphy Randle <murphyspublic@gmail.com>
2012-11-16 22:27:15 -0800

    Added classes and javadoc for data access patterns

---
+++ src/model/storage/IDAOFactory.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package model.storage;
+
+/**
+ * @author murphyra
+ *
+ */
+public interface IDAOFactory {
+	
+	/**
+	 * @return an item data access object.
+	 * The implementation of the DAO is defined by the implementation of this interface.
+	 */
+	public IStorageDAO getItemDAO();
+	
+	/**
+	 * @return a product data access object.
+	 * The implementation of the DAO is defined by the implementation of this interface.
+	 */
+	public IStorageDAO getProductDAO();
+	
+	/**
+	 * @return a product group data access object.
+	 * The implementation of the DAO is defined by the implementation of this interface.
+	 */
+	public IStorageDAO getProductGroupDAO();
+	
+	/**
+	 * @return a storage unit data access object.
+	 * The implementation of the DAO is defined by the implementation of this interface.
+	 */
+	public IStorageDAO getStorageUnitDAO();
+
+}
---
+++ src/model/storage/ProductDAO.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package model.storage;
+
+import model.common.IModel;
+
+import common.Result;
+
+/**
+ * @author murphyra
+ *
+ */
+public class ProductDAO implements IStorageDAO {
+
+	/* (non-Javadoc)
+	 * @see model.storage.IStorageDAO#insert(model.common.IModel)
+	 */
+	@Override
+	public Result insert(IModel model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see model.storage.IStorageDAO#update(model.common.IModel)
+	 */
+	@Override
+	public Result update(IModel model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see model.storage.IStorageDAO#delete(model.common.IModel)
+	 */
+	@Override
+	public Result delete(IModel model) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see model.storage.IStorageDAO#get(int)
+	 */
+	@Override
+	public IModel get(int id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
