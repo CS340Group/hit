b98c0ce2d93e1dd12eafc7b093bd5561780a6a01
Nick Ethier <ncethier@gmail.com>
2012-11-29 01:15:08 -0700

    fix sql

--- buildDB.sql
+++ buildDB.sql
@@ -1,5 +1,4 @@
 CREATE TABLE "item" ("id" INTEGER,"productId" INTEGER,"barcode" TEXT,"entryTime" LONG,"exitTime" LONG,"deleted" BOOL);
 CREATE TABLE "productGroup" ("id" INTEGER, "name" TEXT, "rootParentId" INTEGER, "parentId" INTEGER, "3MonthSupplyAmount" FLOAT, "3MonthSupplyUnit" TEXT);
 CREATE TABLE "storageUnit" ("id" INTEGER, "name" TEXT, "rootParentId" INTEGER);
-CREATE TABLE "item" ("id" INTEGER,"productId" INTEGER,"barcode" TEXT,"entryTime" LONG,"exitTime" LONG,"deleted" BOOL)
-CREATE TABLE "product" ("id" INTEGER UNIQUE , "storageUnitId" INTEGER, "parentId" INTEGER, "barcode" TEXT, "3MonthSupply" INTEGER, "sizeAmount" FLOAT, "sizeUnit" TEXT, "deleted" BOOL, "description" TEXT, "shelfLife" INTEGER, "creationDate" LONG)
+CREATE TABLE "product" ("id" INTEGER UNIQUE , "storageUnitId" INTEGER, "parentId" INTEGER, "barcode" TEXT, "3MonthSupply" INTEGER, "sizeAmount" FLOAT, "sizeUnit" TEXT, "deleted" BOOL, "description" TEXT, "shelfLife" INTEGER, "creationDate" LONG);
