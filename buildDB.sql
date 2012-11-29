4f29a23e3ceef4942956e3602b70b6dbe7a4a2b4
Nick Ethier <ncethier@gmail.com>
2012-11-29 00:16:47 -0700

    oops

--- buildDB.sql
+++ buildDB.sql
@@ -1,3 +1,3 @@
 CREATE TABLE "item" ("id" INTEGER,"productId" INTEGER,"barcode" TEXT,"entryTime" LONG,"exitTime" LONG,"deleted" BOOL);
-CREATE TABLE "productGroup" ("id" INTEGER, "name" TEXT, "rootParentId" INTEGER, "parentId" INTEGER, "3MonthSupplyAmount" LONG, "3MonthSupplyUnit" TEXT);
+CREATE TABLE "productGroup" ("id" INTEGER, "name" TEXT, "rootParentId" INTEGER, "parentId" INTEGER, "3MonthSupplyAmount" FLOAT, "3MonthSupplyUnit" TEXT);
 CREATE TABLE "storageUnit" ("id" INTEGER, "name" TEXT, "rootParentId" INTEGER);
