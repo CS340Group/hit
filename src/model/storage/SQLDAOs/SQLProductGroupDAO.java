650b193ba5622ca48141f61e6bea345af2e81b49
Nick Ethier <ncethier@gmail.com>
2012-11-28 23:54:42 -0700

    pg and su sqlDAO start

--- src/model/storage/SQLDAOs/SQLProductGroupDAO.java
+++ src/model/storage/SQLDAOs/SQLProductGroupDAO.java
@@ -4,22 +4,45 @@
 package model.storage.SQLDAOs;
 
 import model.common.IModel;
+import model.common.Size;
+import model.productcontainer.ProductGroup;
 import model.storage.IStorageDAO;
 
 import common.Result;
+import model.storage.SQLDAOFactory;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Provides the functionality of accessing the stored information for a product group.
  */
 public class SQLProductGroupDAO implements IStorageDAO {
 
-	/* (non-Javadoc)
+    private SQLDAOFactory _factory = new SQLDAOFactory();
+
+    /* (non-Javadoc)
 	 * @see model.storage.IStorageDAO#insert(model.common.IModel)
 	 */
 	@Override
 	public Result insert(IModel model) {
-		// TODO Auto-generated method stub
-		return null;
+        PreparedStatement statement;
+        ProductGroup pg = (ProductGroup) model;
+        try {
+            String query = "INSERT INTO productGroup (id,name,rootParentId,parentId,3MonthSupplyAmount, 3MonthSupplyUnit) VALUES (?,?,?,?,?,?);";
+            statement = _factory.getConnection().prepareStatement(query);
+            statement.setInt(1, pg.getId());
+            statement.setString(2, pg.getName());
+            statement.setInt(3, pg.getRootParentId());
+            statement.setInt(4, pg.getParentId());
+            statement.setFloat(5, pg.get3MonthSupply().getAmount());
+            statement.setString(6, pg.get3MonthSupply().getUnit());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            return new Result(false, e.getMessage());
+        }
+        return new Result(true);
 	}
 
 	/* (non-Javadoc)
@@ -27,8 +50,22 @@ public class SQLProductGroupDAO implements IStorageDAO {
 	 */
 	@Override
 	public Result update(IModel model) {
-		// TODO Auto-generated method stub
-		return null;
+        PreparedStatement statement;
+        ProductGroup pg = (ProductGroup) model;
+        try {
+            String query = "UPDATE storageUnit SET name=?,rootParentId=?,parentId=?,3MonthSupplyAmount=?,3MonthSupplyUnit=? where id=?";
+            statement = _factory.getConnection().prepareStatement(query);
+            statement.setString(1, pg.getName());
+            statement.setInt(2, pg.getRootParentId());
+            statement.setInt(3, pg.getParentId());
+            statement.setFloat(4, pg.get3MonthSupply().getAmount());
+            statement.setString(5, pg.get3MonthSupply().getUnit());
+            statement.setInt(6, pg.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            return new Result(false, e.getMessage());
+        }
+        return new Result(true);
 	}
 
 	/* (non-Javadoc)
@@ -36,8 +73,17 @@ public class SQLProductGroupDAO implements IStorageDAO {
 	 */
 	@Override
 	public Result delete(IModel model) {
-		// TODO Auto-generated method stub
-		return null;
+        PreparedStatement statement;
+        ProductGroup pg = (ProductGroup) model;
+        try {
+            String query = "DELETE FROM storageUnit WHERE id=?;";
+            statement = _factory.getConnection().prepareStatement(query);
+            statement.setInt(1, pg.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            return new Result(false, e.getMessage());
+        }
+        return new Result(true);
 	}
 
 	/* (non-Javadoc)
@@ -45,8 +91,26 @@ public class SQLProductGroupDAO implements IStorageDAO {
 	 */
 	@Override
 	public IModel get(int id) {
-		// TODO Auto-generated method stub
-		return null;
+        PreparedStatement statement;
+        ProductGroup pg = null;
+        try {
+            String query = "SELECT name,rootParentId,parentId,3MonthSupplyAmount,3MonthSupplyUnit WHERE id=?;";
+            statement = _factory.getConnection().prepareStatement(query);
+            statement.setInt(1, id);
+            ResultSet rSet = statement.executeQuery();
+            while(rSet.next()){
+                pg = new ProductGroup();
+                pg.setId(id);
+                pg.setName(rSet.getString(1));
+                pg.setRootParentId(rSet.getInt(2));
+                pg.setParentId(rSet.getInt(3));
+                pg.set3MonthSupply(new Size(rSet.getFloat(4), rSet.getString(5)));
+                pg.setValid(true);
+            }
+        } catch (SQLException e) {
+            return null;
+        }
+        return pg;
 	}
 
 	@Override
--- src/model/storage/SQLDAOs/SQLStorageUnitDAO.java
+++ src/model/storage/SQLDAOs/SQLStorageUnitDAO.java
@@ -4,22 +4,41 @@
 package model.storage.SQLDAOs;
 
 import model.common.IModel;
+import model.productcontainer.StorageUnit;
 import model.storage.IStorageDAO;
 
 import common.Result;
+import model.storage.SQLDAOFactory;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Provides the functionality of accessing the stored information for a storage unit.
  */
 public class SQLStorageUnitDAO implements IStorageDAO {
 
+    private SQLDAOFactory _factory = new SQLDAOFactory();
+
 	/* (non-Javadoc)
 	 * @see model.storage.IStorageDAO#insert(model.common.IModel)
 	 */
 	@Override
 	public Result insert(IModel model) {
-		// TODO Auto-generated method stub
-		return null;
+        PreparedStatement statement;
+        StorageUnit su = (StorageUnit) model;
+        try {
+            String query = "INSERT INTO storageUnit (id,name,rootParentId) VALUES (?,?,?);";
+            statement = _factory.getConnection().prepareStatement(query);
+            statement.setInt(1, su.getId());
+            statement.setString(2, su.getName());
+            statement.setInt(3, su.getRootParentId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            return new Result(false, e.getMessage());
+        }
+        return new Result(true);
 	}
 
 	/* (non-Javadoc)
@@ -27,8 +46,19 @@ public class SQLStorageUnitDAO implements IStorageDAO {
 	 */
 	@Override
 	public Result update(IModel model) {
-		// TODO Auto-generated method stub
-		return null;
+        PreparedStatement statement;
+        StorageUnit su = (StorageUnit) model;
+        try {
+            String query = "UPDATE storageUnit SET name=?,rootParentId=? where id=?";
+            statement = _factory.getConnection().prepareStatement(query);
+            statement.setString(1, su.getName());
+            statement.setInt(2, su.getRootParentId());
+            statement.setInt(3, su.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            return new Result(false, e.getMessage());
+        }
+        return new Result(true);
 	}
 
 	/* (non-Javadoc)
@@ -36,8 +66,17 @@ public class SQLStorageUnitDAO implements IStorageDAO {
 	 */
 	@Override
 	public Result delete(IModel model) {
-		// TODO Auto-generated method stub
-		return null;
+        PreparedStatement statement;
+        StorageUnit su = (StorageUnit) model;
+        try {
+            String query = "DELETE FROM storageUnit WHERE id=?;";
+            statement = _factory.getConnection().prepareStatement(query);
+            statement.setInt(1, su.getId());
+            statement.executeUpdate();
+        } catch (SQLException e) {
+            return new Result(false, e.getMessage());
+        }
+        return new Result(true);
 	}
 
 	/* (non-Javadoc)
@@ -45,8 +84,24 @@ public class SQLStorageUnitDAO implements IStorageDAO {
 	 */
 	@Override
 	public IModel get(int id) {
-		// TODO Auto-generated method stub
-		return null;
+        PreparedStatement statement;
+        StorageUnit su = null;
+        try {
+            String query = "SELECT name,rootParentId WHERE id=?;";
+            statement = _factory.getConnection().prepareStatement(query);
+            statement.setInt(1, id);
+            ResultSet rSet = statement.executeQuery();
+            while(rSet.next()){
+                su = new StorageUnit();
+                su.setId(id);
+                su.setName(rSet.getString(1));
+                su.setRootParentId(rSet.getInt(2));
+                su.setValid(true);
+            }
+        } catch (SQLException e) {
+            return null;
+        }
+        return su;
 	}
 
 	@Override
